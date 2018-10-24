d2077f99390bf11341ffbed245807772e515eb25
Kevin <krijnders@outlook.cim>
2018-10-24 20:42:55 +0200

    PDO begin

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
  </head>
  <body>
  <?php 
-    echo '<p>Hello World!</p>'; 
-
+   echo "<pre><br>";
+   $pdo = new PDO('mysql:host=localhost;port=3306;dbname=test', 'root', 'root');
+   $stmt = $pdo->query("SELECT * FROM users");
+   while ( $row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+       print_r($row);
+   }
+   echo "</pre>";
  ?> 
  </body>
 </html>
